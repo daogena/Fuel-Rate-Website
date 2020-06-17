672aef760264cc2ad7a02928e86ebd83a9f67272
daogena <66931735+daogena@users.noreply.github.com>
2020-06-16 22:40:23 -0500

    login.js-update1

--- js/login.js
+++ js/login.js
@@ -1,1 +1,12 @@
 // Login page javascript
+function login() {
+    var username = document.getElementById("username-input").value;
+    var password = document.getElementById("password-input").value;
+    var loginError = document.getElementById("login-error-msg"); 
+
+    if (username == "user" && password == "pass") {
+        alert("You have successfully logged in.");
+    } else {
+        loginError.style.opacity = 1; 
+    }
+}
