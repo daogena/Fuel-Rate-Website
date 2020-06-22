1dbae216621cb8f9d35b04fa590add4fe99d830c
daogena <66931735+daogena@users.noreply.github.com>
2020-06-21 21:09:08 -0500

    fuel-form.js-update1

--- js/fuel-form.js
+++ js/fuel-form.js
@@ -1,1 +1,15 @@
 // Fuel form javascript
+function calculate() {
+    let gallons = document.getElementById("gallons").value; 
+    let date = document.querySelector('input[type=date]');
+    let loginError = document.getElementById("login-error-msg"); 
+    let priceInString = document.getElementById("suggested-price-per-gallon").value.substring(1); 
+    let price = parseInt(priceInString, 10);
+
+    if ((gallons == "")  || (date.value == "")) {
+        loginError.style.opacity = 1; 
+    } else {
+        let total = gallons * price; 
+        document.getElementById("total").innerHTML = "$" + total; 
+    }
+}
