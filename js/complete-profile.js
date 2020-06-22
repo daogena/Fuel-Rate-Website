11f91a4640858b158d2426b5add2ceac6e0a715d
daogena <66931735+daogena@users.noreply.github.com>
2020-06-21 21:08:43 -0500

    complete-profile.js-update1

--- js/complete-profile.js
+++ js/complete-profile.js
@@ -1,1 +1,16 @@
 // Profile page javascript
+function submit() {
+    let fullName = document.getElementById("full-name").value;
+    let address = document.getElementById("address-1").value; 
+    let city = document.getElementById("city").value; 
+    let state = document.getElementById("state").value; 
+    let zipCode = document.getElementById("zipcode").value;
+    let loginError = document.getElementById("login-error-msg"); 
+
+    if ((fullName == "") || (address == "") || (city == "") || (state == "State") || (zipCode.length < 5)) {
+        loginError.style.opacity = 1; 
+    }
+    else {
+        window.location.href = "profile.html";
+    }
+}
