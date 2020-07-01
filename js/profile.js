98f8800065183b7bcc4243525c1649490ec3d59a
daogena <66931735+daogena@users.noreply.github.com>
2020-07-01 17:14:46 -0500

    Create profile.js

---
+++ js/profile.js
@@ -0,0 +1,34 @@
+const API_URL = "http://localhost:3000/api/users/";
+const API_BASE_URL = "http://localhost:3000/";
+
+window.onload = () => {
+    getUserProfile(); 
+    getUserIdParam(); 
+}
+
+const getUserIdParam = () => {
+    const queryString = window.location.search; 
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get("id");
+}
+
+const getUserProfile = () => {
+    const userId = getUserIdParam(); 
+    const url = `${API_URL}${userId}`; 
+    fetch(url, {
+        method: 'GET'
+    }).then((response) => {
+        return response.json(); 
+    }).then((data) => {
+        buildProfile(data);
+    });
+}
+
+const buildProfile = (data) => {
+    document.getElementById("full-name").innerText = data.fullName; 
+    document.getElementById("address").innerText = data.address['line1']; 
+    document.getElementById("city").innerText = data.address['city'] + ","; 
+    document.getElementById("state").innerText = data.address['state']; 
+    document.getElementById("zipcode").innerText = data.address['zipcode']; 
+}
+
