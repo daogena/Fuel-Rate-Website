759abaccc842cc28f06e957f8ba7d76b102a9f78
daogena <66931735+daogena@users.noreply.github.com>
2020-07-01 17:15:17 -0500

    register.js-update1

--- js/register.js
+++ js/register.js
@@ -1,1 +1,29 @@
 // Registration page javascript
+
+const API_URL = "http://localhost:3000/api/users";
+
+const registerUser = () => {
+    const username = document.getElementById("username-input").value;
+    const password = document.getElementById("password-input").value;
+    // const confirmed = document.getElementById("confirm-password-input").value; 
+    // var loginError = document.getElementById("login-error-msg"); 
+
+    // if (pass != confirmed) {
+    //     loginError.style.opacity = 1; 
+    // } else {
+        let data = new FormData(); 
+        data.append("username", username);
+        data.append("password", password);
+
+        fetch(API_URL, {
+            method: 'POST',  
+            body: data
+        }).then(() => {
+            // setTimeout(() => {
+            //     window.location.href = "login.html";
+            // }, 1000)
+            console.log("We are good");
+        })
+    // }
+
+}
