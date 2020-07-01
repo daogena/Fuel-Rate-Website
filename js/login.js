5d3301136f1f8c5c55ceb54164b65336232cbaed
daogena <66931735+daogena@users.noreply.github.com>
2020-07-01 17:14:13 -0500

    login.js-update2

--- js/login.js
+++ js/login.js
@@ -1,12 +1,36 @@
-// Login page javascript
-function login() {
-    var username = document.getElementById("username-input").value;
-    var password = document.getElementById("password-input").value;
-    var loginError = document.getElementById("login-error-msg"); 
+const API_URL = "http://localhost:3000/api/users";
+const API_BASE_URL = "http://localhost:3000/";
 
-    if (username == "user" && password == "pass") {
-        alert("You have successfully logged in.");
-    } else {
-        loginError.style.opacity = 1; 
-    }
+var username;
+var password;
+var loginError; 
+
+function login() {
+    username = document.getElementById("username-input").value;
+    password = document.getElementById("password-input").value;
+    loginError = document.getElementById("login-error-msg"); 
+    getUsers();
 }
+
+const getUsers = () => {
+    fetch(API_URL, {
+        method: 'GET'
+    }).then((response) => {
+        return response.json();
+    }).then((data) => {
+        findUser(data);
+    })
+}
+
+const findUser = (userList) => {
+    for (user of userList) {
+        if (user.username == username && user.password == password) {
+            const profileLink = `./profile.html?id=${user.username}`;
+            window.location.href = `${profileLink}`;
+        }
+    }
+    loginError.style.opacity = 1; 
+}
+
+
+
