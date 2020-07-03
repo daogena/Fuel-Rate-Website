74d955335689d9ecc40ec301c609e76f965b0fd6
daogena <66931735+daogena@users.noreply.github.com>
2020-07-03 12:43:46 -0500

    Add files via upload

---
+++ SERVER/app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const User = require("./api/models/users");
+var multer = require('multer');
+
+var upload = multer(); 
+const userData = new User();
+
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*"); 
+    next(); 
+}); 
+
+app.get("/api/users", (req, res) => {
+    res.status(200).send(userData.get())
+});
+
+app.get("/api/users/:user_id", (req, res) => {
+    const userId = req.params.user_id;
+    const foundUser = userData.getIndividualUser(userId);
+    if (foundUser) {
+        res.status(200).send(foundUser);
+    } else {
+        res.status(404).send("Not found");
+    }
+});
+
+app.post("/api/users", upload.single(), (req, res) => {
+    const newUser = {
+        "username": req.body.username, 
+        "password": req.body.password, 
+        "fullName": "", 
+        "address": "", 
+        "history": ""
+    }
+    userData.add(newUser);
+    res.status(201).send(newUser);
+})
+
+app.listen(3000, () => console.log("Listening on http://localhost:3000"));
