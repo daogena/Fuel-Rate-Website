c9bc6a2bff314fe1a5663e48e4bc31abe83f1a6b
daogena <66931735+daogena@users.noreply.github.com>
2020-07-03 12:42:59 -0500

    Create users.js

---
+++ SERVER/api/models/users.js
@@ -0,0 +1,37 @@
+const PATH = "./data.json"; 
+const fs = require('fs');
+
+
+class User {
+    get() {
+        // Get users
+        return this.readData(); 
+    }
+
+    getIndividualUser() {
+        // Get one user
+        const users = this.readData();
+        const foundUser = users.find((user) => user.username == userId); 
+        return foundUser; 
+    }
+
+    add(newUser) {
+        // Add new user
+        const currentUsers = this.readData(); 
+        currentUsers.unshift(newUser);
+        this.storeData(currentUsers);
+    }
+
+    readData() {
+        let rawdata = fs.readFileSync(PATH);
+        let users = JSON.parse(rawdata);
+        return users; 
+    }
+
+    storeData(rawData) {
+        let data = JSON.stringify(rawData);
+        fs.writeFileSync(PATH, data);
+    }
+}
+
+module.exports = User; 
