f4dd4ae25646375cf8ebd74fdd92e1512fd1459c
daogena <66931735+daogena@users.noreply.github.com>
2020-07-03 12:14:00 -0500

    fuel-form.js-update2-mckintosh-tudee

--- js/fuel-form.js
+++ js/fuel-form.js
@@ -13,3 +13,36 @@ function calculate() {
         document.getElementById("total").innerHTML = "$" + total; 
     }
 }
+
+class pricingModule
+{
+	this.gallonReqested = 0;
+	this.address = 0;
+	this.priceGallon = 0;
+	this.total = 0;
+
+	//functions
+	getGallonRequested = funtion(x) {
+		this.address = x
+	};
+
+	this.getAddress = function(x) {
+
+		this.address = x
+
+	};
+
+	this.getPrice = function(x) { // this stores the price per gallon
+		this.priceGallon = x
+	};
+
+	this.calTotal = function(gallonreq, priceGallon) {
+
+		return this.gallonRequested * this.priceGallon; // retrun the result. the gallon requested times the price per gallon
+	};
+
+
+	this.display = function(x) {
+
+	}; //this function displays all the information colleccted; gallon requested, address, price, and calcultios
+};
