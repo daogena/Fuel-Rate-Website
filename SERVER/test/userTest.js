571d40a979e705b2e537ba45587fdb2a951b9f0c
daogena <66931735+daogena@users.noreply.github.com>
2020-07-04 19:03:36 -0500

    Add files via upload

---
+++ SERVER/test/userTest.js
@@ -0,0 +1,27 @@
+var User = require('../api/models/users');
+var chai = require('chai');
+var expect = chai.expect; 
+const user = new User(); 
+
+chai.should(); 
+
+describe("User class", () => {
+    it('get function should get data', () => {
+        let result = user.get(); 
+        result.should.be.an('array');
+    });
+    it('getIndividualUser function should get user', () => {
+        let userId = "user";
+        let result = user.getIndividualUser(userId); 
+        result.should.be.an('object');
+    })
+    it('add function should return undefined', () => {
+        let newUser = "username";
+        let result = user.add(newUser);
+        expect(result).to.be.an('undefined');
+    })
+    it('readData function should read data', () => {
+        let result = user.readData(); 
+        result.should.be.an('array');
+    });
+});
