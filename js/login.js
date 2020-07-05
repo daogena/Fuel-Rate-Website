a7c20f2be99411719237154e7ee3f0f6574efd6c
daogena <66931735+daogena@users.noreply.github.com>
2020-07-04 19:10:31 -0500

    login.js-comments

--- js/login.js
+++ js/login.js
@@ -5,13 +5,17 @@ var username;
 var password;
 var loginError; 
 
+// Login function is called when user clicks the login button 
 function login() {
+    // Gets user input for username and password
     username = document.getElementById("username-input").value;
     password = document.getElementById("password-input").value;
     loginError = document.getElementById("login-error-msg"); 
     getUsers();
 }
 
+// getUsers function fetches all users from API
+// calls findUser to find a user that matches user input
 const getUsers = () => {
     fetch(API_URL, {
         method: 'GET'
@@ -23,12 +27,20 @@ const getUsers = () => {
 }
 
 const findUser = (userList) => {
+    // Goes through database by each entry
     for (user of userList) {
-        if (user.username == username && user.password == password) {
+        // If username and password match but fullName is empty, redirect user to complete their profile
+        if (user.username == username && user.password == password && user.fullName == "") {
+            const completeProfileLink = `./complete-profile.html?id=${user.username}`; 
+            window.location.href = `${completeProfileLink}`
+        }
+        // If username and password match, redirect user to their profile
+        else if (user.username == username && user.password == password) {
             const profileLink = `./profile.html?id=${user.username}`;
             window.location.href = `${profileLink}`;
         }
     }
+    // If username or password don't match, show error message
     loginError.style.opacity = 1; 
 }
 
