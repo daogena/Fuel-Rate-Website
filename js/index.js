92bab497343603ea0c541f77824a9ee7d90a84c9
daogena <66931735+daogena@users.noreply.github.com>
2020-07-09 21:43:49 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,48 +1,56 @@
-const API_URL = "http://localhost:3000/api/users";
+
+
+const API_URL = "http://localhost:3000/api/users/";
 const API_BASE_URL = "http://localhost:3000/";
 
-var username;
-var password;
+var username = "placeholder";
+var password = "placeholder";
 var loginError; 
 
-// Login function is called when user clicks the login button 
 function login() {
-    // Gets user input for username and password
-    username = document.getElementById("username-input").value;
+    username = document.getElementById("username-input").value; 
     password = document.getElementById("password-input").value;
     loginError = document.getElementById("login-error-msg"); 
-    getUsers();
+    findUser(); 
 }
 
-// getUsers function fetches all users from API
-// calls findUser to find a user that matches user input
-const getUsers = () => {
-    fetch(API_URL, {
+function findUser() {
+    let data = new FormData(); 
+    data.append("username", username); 
+    data.append("password", password); 
+
+    const url = `${API_URL}${username}`; 
+    fetch(url, {
+        method: 'POST', 
+        body: data
+    }).then((response) => {
+        if (response.status == 200) {
+            getUser(); 
+        }
+        else {
+            loginError.style.opacity = 1; 
+        }
+    })
+}
+
+function getUser() {
+    const url = `${API_URL}${username}`;  
+    fetch(url, {
         method: 'GET'
     }).then((response) => {
         return response.json();
     }).then((data) => {
-        findUser(data);
+        redirectUser(data);
     })
 }
 
-const findUser = (userList) => {
-    // Goes through database by each entry
-    for (user of userList) {
-        // If username and password match but fullName is empty, redirect user to complete their profile
-        if (user.username == username && user.password == password && user.fullName == "") {
-            const completeProfileLink = `./complete-profile.html?id=${user.username}`; 
-            window.location.href = `${completeProfileLink}`
-        }
-        // If username and password match, redirect user to their profile
-        else if (user.username == username && user.password == password) {
-            const profileLink = `./profile.html?id=${user.username}`;
-            window.location.href = `${profileLink}`;
-        }
+function redirectUser(user) {
+    if (user.fullName == "") {
+        const completeProfileLink = `./complete-profile.html?id=${user.username}`; 
+        window.location.href = `${completeProfileLink}`;
+    } 
+    else {
+        const profileLink = `./profile.html?id=${user.username}`;
+        window.location.href = `${profileLink}`;
     }
-    // If username or password don't match, show error message
-    loginError.style.opacity = 1; 
 }
-
-
-
