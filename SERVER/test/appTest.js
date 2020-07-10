64c259e0bcac218aebc43ade37c7357a5cf3648d
daogena <66931735+daogena@users.noreply.github.com>
2020-07-09 21:42:58 -0500

    Update appTest.js

--- SERVER/test/appTest.js
+++ SERVER/test/appTest.js
@@ -5,7 +5,7 @@ var chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 chai.should(); 
 
-describe("Users", () => {
+describe("App", () => {
     //Test to get all user data
     var server = chai.request.agent(app);
     it ("should get all user data", (done) => {
@@ -26,7 +26,7 @@ describe("Users", () => {
                 done(); 
             })
     });
-    it("should not get individual user", (done) => {
+    it("should not get nonexistent user", (done) => {
         const id = "failUser"; 
         server
             .get(`/api/users/${id}`)
@@ -34,5 +34,71 @@ describe("Users", () => {
                 res.should.have.status(404);
                 done(); 
             }) 
-    })
+    });
+    it("should update existing user", (done) => {
+        const id = "user"; 
+        let newUser = {
+            "fullName": "Gena Dao"
+        }; 
+        server
+            .put(`/api/users/${id}`)
+            .send(newUser)
+            .end((err, res) => {
+                res.should.have.status(200);
+                done(); 
+            })
+    });
+    it("should not update user not found", (done) => {
+        const id = "sampleuser";
+        let newUser = {
+            "fullName": "Gena Dao"
+        };
+        server
+            .put(`/api/users/${id}`)
+            .send(newUser)
+            .end((err, res) => {
+                res.should.have.status(404);
+                done(); 
+            }) 
+    });
+    it("should return 200 status for correct password", (done) => {
+        const id = "user"; 
+        let user = {
+            "username": "user", 
+            "password": "pass"
+        }; 
+        server
+            .post(`/api/users/${id}`)
+            .send(user)
+            .end((err, res) => {
+                res.should.have.status(200);
+                done(); 
+            })
+    });
+    it("should return 404 status for incorrect password", (done) => {
+        const id = "user"; 
+        let user = {
+            "username": "user", 
+            "password": "incorrectpass"
+        }; 
+        server
+            .post(`/api/users/${id}`)
+            .send(user)
+            .end((err, res) => {
+                res.should.have.status(404);
+                done(); 
+            })
+    });
+    it("should add new user", (done) => {
+        let newUser = {
+            "username": "user"
+        }; 
+        server 
+            .post('/api/users')
+            .send(newUser)
+            .end((err, res) => {
+                res.should.have.status(201); 
+                done(); 
+            })
+    });
 });
