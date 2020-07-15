7194af4683055a04aaf9600877ec978b02c378a1
daogena <66931735+daogena@users.noreply.github.com>
2020-07-15 12:27:44 -0500

    Update fuel-form.js

--- js/fuel-form.js
+++ js/fuel-form.js
@@ -1,48 +1,104 @@
-// Fuel form javascript
-function calculate() {
-    let gallons = document.getElementById("gallons").value; 
-    let date = document.querySelector('input[type=date]');
-    let loginError = document.getElementById("login-error-msg"); 
-    let priceInString = document.getElementById("suggested-price-per-gallon").value.substring(1); 
-    let price = parseInt(priceInString, 10);
+const API_URL = "http://localhost:3000/api/users/";
+const API_FORM_URL = "http://localhost:3000/api/users/form/";
 
-    if ((gallons == "")  || (date.value == "")) {
-        loginError.style.opacity = 1; 
-    } else {
-        let total = gallons * price; 
-        document.getElementById("total").innerHTML = "$" + total; 
-    }
+window.onload = () => {
+    getUserIdParam(); 
 }
 
-class pricingModule
-{
-	this.gallonReqested = 0;
-	this.address = 0;
-	this.priceGallon = 0;
-	this.total = 0;
+function getUserIdParam() {
+    const queryString = window.location.search; 
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get("id");
+}
 
-	//functions
-	getGallonRequested = funtion(x) {
-		this.address = x
-	};
+// Set current price and company profit factor
+let currentPrice = 1.5;
+let profitFactor = 0.1;
 
-	this.getAddress = function(x) {
+// Get individual user's data
+function getUserHistory() {
+    const userId = getUserIdParam(); 
+    const url = `${API_URL}${userId}`; 
+    fetch(url, {
+        method: 'GET'
+    }).then((response) => {
+        return response.json(); 
+    }).then((data) => {
+        pricingModule(data);
+    });
+}
 
-		this.address = x
+const pricingModule = (user) => {
+    // Set Location Factor depending on whether user is in- or out-of-state
+    let state = user.address['state']; 
+    let locationFactor; 
+    if (state == 'TX') {
+        locationFactor = 0.02; 
+    } else {
+        locationFactor = 0.04; 
+    }
+    
+    // Set Rate History Factor depending on whether user has requested fuel before
+    let historyFactor; 
+    let historyList = user.history; 
+    let count = 0; 
+    for(entry of historyList) {
+        count++;
+    }
+    if (count > 0) {
+        historyFactor = 0.01;
+    } else {
+        historyFactor = 0; 
+    }
+    
+    // Set Gallons Request Factor 
+    let gallons = document.getElementById("gallons").value; 
+    let gallonFactor; 
+    if (gallons > 1000) {
+        gallonFactor = 0.02;
+    } else {
+        gallonFactor = 0.03;
+    }
 
-	};
+    // Calculate margin
+    let margin = currentPrice * (locationFactor - historyFactor + gallonFactor + profitFactor); 
+    margin.toFixed(5); 
+    
+    // Calculate Suggested price / gallon 
+    // Display suggested price as read only 
+    let suggestedPrice = currentPrice + margin; 
+    document.getElementById("suggested-price-per-gallon").value = `$${suggestedPrice}`; 
 
-	this.getPrice = function(x) { // this stores the price per gallon
-		this.priceGallon = x
-	};
+    // Calculate total
+    // Display total as currency
+    let total = gallons * suggestedPrice; 
+    total = total.toLocaleString("en-US", { 
+        style: "currency", 
+        currency: "USD"
+    }); 
+    document.getElementById("total").innerHTML = `${total}`; 
+}
 
-	this.calTotal = function(gallonreq, priceGallon) {
+// Add new history entry 
+function submitForm() {
+    let total = document.getElementById("total").innerHTML; 
+    let gallons = document.getElementById("gallons").value; 
+    let deliveryDate = document.querySelector('input[type=date]').value;
+    let suggestedPrice = document.getElementById("suggested-price-per-gallon").value; 
 
-		return this.gallonRequested * this.priceGallon; // retrun the result. the gallon requested times the price per gallon
-	};
+    let data = new FormData(); 
+    data.append("total", total); 
+    data.append("gallons", gallons); 
+    data.append("deliveryDate", deliveryDate); 
+    data.append("price", suggestedPrice); 
 
+    const userId = getUserIdParam(); 
+    const url = `${API_FORM_URL}${userId}`; 
 
-	this.display = function(x) {
-
-	}; //this function displays all the information colleccted; gallon requested, address, price, and calcultios
-};
+    fetch(url, {
+        method: 'PUT', 
+        body: data
+    }).then(() => {
+        window.location.reload(); 
+    })
+}
