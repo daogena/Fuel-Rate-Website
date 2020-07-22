7786e217879365dc327da237de7105a285153548
TudeeMcKintosh <54680698+TudeeMcKintosh@users.noreply.github.com>
2020-07-22 01:26:38 -0500

    Update fuel-form.js
    most of the changes that i did were on gallons request factor. I validated that the user gallon request is at least greater than 1000 or less than 1000. Because  the user may input  something non-sensible like a negative number or characters for the gallons request. This is safe software design practice to protect our program from hackers. I learned this is network security.
    
    some of the other changes that I did were just comments on the codes you wrote. 
    
    please look at these changes and update our assignment

--- js/fuel-form.js
+++ js/fuel-form.js
@@ -46,19 +46,39 @@ const pricingModule = (user) => {
         count++;
     }
     if (count > 0) {
-        historyFactor = (1/100);
+        historyFactor = (1/100); // if clent request fuel before
     } else {
-        historyFactor = 0; 
+        historyFactor = 0; //if no history
     }
     
     // Set Gallons Request Factor 
-    let gallons = document.getElementById("gallons").value; 
-    let gallonFactor; 
-    if (gallons > 1000) {
-        gallonFactor = (2/100);
-    } else {
-        gallonFactor = (3/100);
-    }
+    let gallons_valid = false; // holds true/false if a valid input for gallons was received. initially set to false to help with do-while loop
+    
+    do 
+    {
+        let gallons = document.getElementById("gallons").value; 
+        let gallonFactor; 
+
+        if( gallon == 0)//if we get zero as gallon amount
+        {
+            gallons_valid = false; // we need a gallon amount to do the calculation. we can't proceed with zero gallon as entry. so we ask for a new entry again   
+        }
+        else if (gallons > 1000)  // checks wheter gallon amount is greater than 1000
+        {      
+            gallonFactor = (2/100);// if gallon amount greater than 1000. gallon request factor = 2%
+            gallons_valid = true; // we have a valid input for gallon. helps exist do-while loop
+        } 
+        else if (gallons < 1000) // checks if gallon amount is less than 1000
+        {
+            gallonFactor = (3/100);// gallons factor is 3% if gallon amount less than 1000
+            gallons_valid = true; //we have a valid input for gallon. we set this to true to help exit do-while loop
+        }
+        else // if the gallon entry is not less 1000, or greater than 1000, or at equal 0. then we most likely have a non-sensible entry which can cause damage to our program
+        {
+                gallons_valid = false;// we most likely have a nonsible entry then. and so the input is not valid
+        }
+
+    }while(gallons_valid == false)// we repeatidly attemp to set the gallon factor untill we get a valid input.
 
     // Calculate margin
     let margin = currentPrice * (locationFactor - historyFactor + gallonFactor + profitFactor); 
